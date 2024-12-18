97915b2f9b1093b692311f4b2daa7878f58c6fce
SantiOlvera <98351363+SantiOlvera@users.noreply.github.com>
2024-12-18 16:23:17 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,202 @@
+import asyncio
+import time
+import os
+import shutil
+import subprocess
+import requests
+import streamlit as st
+from dotenv import load_dotenv
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+
+from deepgram import (
+    DeepgramClient,
+    DeepgramClientOptions,
+    LiveTranscriptionEvents,
+    LiveOptions,
+    Microphone,
+)
+
+# Load environment variables
+load_dotenv()
+
+class LanguageModelProcessor:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY"))
+        self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+        with open('system_prompt_en.txt', 'r') as file:
+            system_prompt = file.read().strip()
+        
+        self.prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template("{text}")
+        ])
+
+        self.conversation = LLMChain(
+            llm=self.llm,
+            prompt=self.prompt,
+            memory=self.memory
+        )
+
+    def process(self, text):
+        self.memory.chat_memory.add_user_message(text)
+
+        start_time = time.time()
+
+        response = self.conversation.invoke({"text": text})
+        end_time = time.time()
+
+        self.memory.chat_memory.add_ai_message(response['text'])
+
+        elapsed_time = int((end_time - start_time) * 1000)
+        return response['text']
+
+class TextToSpeech:
+    DG_API_KEY = os.getenv("DEEPGRAM_API_KEY")
+    MODEL_NAME = "aura-asteria-en"
+
+    @staticmethod
+    def is_installed(lib_name: str) -> bool:
+        return shutil.which(lib_name) is not None
+
+    def speak(self, text):
+        if not self.is_installed("ffplay"):
+            raise ValueError("ffplay not found, necessary to stream audio.")
+
+        DEEPGRAM_URL = f"https://api.deepgram.com/v1/speak?model={self.MODEL_NAME}"
+        headers = {
+            "Authorization": f"Token {self.DG_API_KEY}",
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "text": text
+        }
+
+        player_command = ["ffplay", "-autoexit", "-", "-nodisp"]
+        player_process = subprocess.Popen(
+            player_command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        with requests.post(DEEPGRAM_URL, stream=True, headers=headers, json=payload) as r:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    player_process.stdin.write(chunk)
+                    player_process.stdin.flush()
+
+        player_process.stdin.close()
+        player_process.wait()
+
+class TranscriptCollector:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.transcript_parts = []
+
+    def add_part(self, part):
+        self.transcript_parts.append(part)
+
+    def get_full_transcript(self):
+        return ' '.join(self.transcript_parts)
+
+transcript_collector = TranscriptCollector()
+
+async def get_transcript(callback):
+    transcription_complete = asyncio.Event()
+
+    try:
+        config = DeepgramClientOptions(options={"keepalive": "true"})
+        deepgram = DeepgramClient("", config)
+
+        dg_connection = deepgram.listen.asynclive.v("1")
+
+        async def on_message(self, result, **kwargs):
+            sentence = result.channel.alternatives[0].transcript
+            
+            if not result.speech_final:
+                transcript_collector.add_part(sentence)
+            else:
+                transcript_collector.add_part(sentence)
+                full_sentence = transcript_collector.get_full_transcript()
+                if len(full_sentence.strip()) > 0:
+                    full_sentence = full_sentence.strip()
+                    callback(full_sentence)
+                    transcript_collector.reset()
+                    transcription_complete.set()
+
+        dg_connection.on(LiveTranscriptionEvents.Transcript, on_message)
+
+        options = LiveOptions(
+            model="nova-2",
+            punctuate=True,
+            language="en",
+            encoding="linear16",
+            channels=1,
+            sample_rate=16000,
+            endpointing=300,
+            smart_format=True,
+        )
+
+        await dg_connection.start(options)
+
+        microphone = Microphone(dg_connection.send)
+        microphone.start()
+
+        await transcription_complete.wait()
+
+        microphone.finish()
+
+        await dg_connection.finish()
+
+    except Exception as e:
+        print(f"Could not open socket: {e}")
+        return
+
+class ConversationManager:
+    def __init__(self):
+        self.transcription_response = ""
+        self.llm = LanguageModelProcessor()
+
+    async def main(self):
+        def handle_full_sentence(full_sentence):
+            self.transcription_response = full_sentence
+
+        while True:
+            await get_transcript(handle_full_sentence)
+            
+            if "goodbye" in self.transcription_response.lower():
+                break
+
+            llm_response = self.llm.process(self.transcription_response)
+            tts = TextToSpeech()
+            tts.speak(llm_response)
+
+            self.transcription_response = ""
+
+def run_conversation():
+    manager = ConversationManager()
+    asyncio.run(manager.main())
+
+# Streamlit UI
+st.title("Voice Assistant with Language Model and Text-to-Speech")
+
+if st.button("Start Conversation"):
+    st.write("Listening for speech input...")
+    run_conversation()
+
+st.text_area("LLM Response", height=200)
+
---
+++ parser_pdf.py
@@ -0,0 +1,231 @@
+from PyPDF2 import PdfReader
+from groq import Groq
+import re
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+import yfinance as yf
+import datetime
+import pandas as pd
+from langchain_groq import ChatGroq
+
+
+load_dotenv()
+
+# Función para extraer texto de PDFs
+def extract_text_from_pdf(pdf_path):
+    reader = PdfReader(pdf_path)
+    text = ''
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+path='pdf/cover.pdf'
+
+text0 = extract_text_from_pdf(path)
+
+path='pdf/wall_street.pdf'
+
+text=extract_text_from_pdf(path)
+
+# In[ ]:
+
+chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768",max_tokens=256, timeout=None, max_retries=3, groq_api_key=os.getenv("GROQ_API_KEY"))
+
+messages = [
+    (
+        "system",
+        "You are an expert in stocks. So you have to find the most interesting articles of the new york times There are a lot of small fragments of news that they are later explained in the article. I need to have the top 3 better articles related to the stock market and u have to indicate how I can identify them. This is baisically by a letter and a number. For example, A2, A3, B1, B2, B3, etc. Be careful with the number of the articles",
+    ),
+    ("human", "Find the most interesting articles of the financial times related to the stock market in this text."),
+    ("human", text0)]
+
+ai_msg = chat.invoke(messages)
+ai_msg.content
+
+messages2 = [
+    (
+        "system",
+        "You are an expert identifiying where to find the article of a related topic. U will hace 3 articles and I can find them by a letter and a number. For example, A1, A2, A3, B1, B2, B3, etc.. The output have to be an array. For example, ['A1', 'B2', 'C3']",
+    ),
+    ("human", "Find how to identify the articles of the financial times related to the stock market in this text."),
+    ("human", ai_msg.content)]
+
+ai_msg2 = chat.invoke(messages2)
+ai_msg2.content
+aux=ai_msg2.content[ai_msg2.content.find('['):ai_msg2.content.find(']')+1]
+
+## get the array of the articles from this format "['A1', 'A6', 'B11']"
+
+aux=aux.replace('[','')
+aux=aux.replace(']','')
+aux=aux.replace('\'','')
+aux=aux.replace(' ','')
+aux=aux.split(',')
+aux
+
+# In[61]:
+
+
+##get the array frome the anserw
+
+aux=ai_msg2.content[ai_msg2.content.find('['):ai_msg2.content.find(']')+1]
+
+## get the array of the articles from this format "['A1', 'A6', 'B11']"
+
+aux=aux.replace('[','')
+aux=aux.replace(']','')
+aux=aux.replace('\'','')
+aux=aux.replace(' ','')
+aux=aux.split(',')
+aux
+
+##if ther is a A1 change to A2
+
+for i in range(len(aux)):
+    if aux[i]=='A1':
+        aux[i]='A2'
+
+
+# In[67]:
+
+
+# Expresión regular para encontrar los encabezados (coincidencias completas)
+pattern = r'(?:[AB]\d+\s*\|\s*)?[A-Z][a-z]+,\s(?:January|February|March|April|May|June|July|August|September|October|November|December)\s(?:[1-9]|[12][0-9]|3[01]),\s2024(?:\s*\|\s*[AB]\d+)?'
+
+# Encontrar todos los encabezados en el texto
+matches = list(re.finditer(pattern, text))
+
+# Extraer el contenido de cada sección correspondiente a las páginas en `aux`
+sections = {}
+for i, match in enumerate(matches):
+    # Obtener el encabezado actual
+    header = match.group(0)
+    
+    # Verificar si el encabezado incluye una página relevante de `aux`
+    if any(page in header for page in aux):
+        # Encontrar el inicio y final de la sección
+        start = match.end()
+        end = matches[i + 1].start() if i + 1 < len(matches) else len(text)
+
+        #print(f"Header: {header}\nContent: {text[start:end].strip()}\n{'-'*40}")
+        
+        # Extraer el contenido entre este encabezado y el siguiente
+        sections[header] = text[start:end].strip()
+
+# In[93]:
+
+
+res=[]
+for i in sections.items():
+    ##tomar el contenido de i[1] y dividirlo en el len entre 4 luego tomar 
+
+
+    if len(i[1])/3.2>5000:
+        texto=i[1][:int(len(i[1])*5000/len(i[1])/3.2)]
+    else:
+        texto=i[1]
+
+    ##numero
+    messages3 = [
+    (
+        "system",
+        "You are an expert identifying relevant the top 3 stocks from the articles provided by the financial times. You have to find the most relevant stocks in the articles. The output has to be an array. For example, ['NVDA', 'AAPL', 'TSLA']",
+    ),
+    ("human", "Find the most relevant stocks in the articles provided by the financial times."),
+    ("human", texto)]
+    ai_msg3 = chat.invoke(messages3)
+    res.append(ai_msg3.content)
+
+# In[ ]:
+
+
+stocks=[]
+for i in res:
+    messages4 = [
+    (
+        "system",
+        "You are an expert identifiying the stocks of a text. U need to put in an array the top 3 stocks of the text. The output has to be like this, ['NVDA', 'AAPL', 'TSLA'], the name of the companies has to be in the bloomberg format.",),
+    ("human", "Find the stocks from the following text an give to me in an array."),
+    ("human", i)]
+    ai_msg4 = chat.invoke(messages4)
+    ai_msg4.content
+    aux2=ai_msg4.content[ai_msg4.content.find('['):ai_msg4.content.find(']')+1]
+    aux2=aux2.replace('[','')
+    aux2=aux2.replace(']','')
+    aux2=aux2.replace('\'','')
+    aux2=aux2.replace(' ','')
+    aux2=aux2.split(',')
+    stocks.append(aux2)
+
+stock=[]
+for i in stocks:
+    for j in i:
+        stock.append(j)
+    
+stock = list(dict.fromkeys(stock))
+
+# In[119]:
+ # Replace with your desired stock symbols
+
+# Define today's date
+date_today = datetime.date.today()
+
+# Dictionary to store DataFrames
+stock_data = {}
+
+# Loop through each stock
+for i in stock:
+    # Download the data
+    data = yf.download(i, start=date_today - datetime.timedelta(days=7), end=date_today)
+    # Extract only the 'Open', 'Low', and 'Close' prices
+    stock_data[i] = data[['Open', 'Low', 'Close']]
+
+
+# In[122]:
+
+
+chat2 = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768",max_tokens=1500, timeout=None, max_retries=3, groq_api_key=os.getenv("GROQ_API_KEY"))
+
+res2=[]
+for i in sections.items():
+    ##tomar el contenido de i[1] y dividirlo en el len entre 4 luego tomar 
+
+
+    if len(i[1])/3.2>5000:
+        texto=i[1][:int(len(i[1])*5000/len(i[1])/3.2)]
+    else:
+        texto=i[1]
+
+    ##numero
+    messages6 = [
+    (
+        "system",
+        "You are an expert in finance, so you have to make a summary of the following text, dont miss the details."),
+    ("human", "Give mi the summary of the following text."),
+    ("human", texto)]
+    ai_msg6 = chat2.invoke(messages6)
+    res2.append(ai_msg6.content)
+
+
+# In[127]:
+
+
+##crear in txt de res2 y de stock.data.items()
+## Poner un mensaje inicila de the news of todays market are the following
+## poner un mensaje final de the stocks related to the news are the following
+
+date_today = datetime.date.today()
+
+
+with open(f"news_{date_today}.txt", "w") as f:
+    f.write("You are a financial expert specializing in investment banking, trading, and market analysis. Your task is to provide an overview of the market today and identify potential market opportunities based on the latest news and stock data provided. Analyze the key events, stock movements, and economic indicators to guide trading strategies and investment decisions.\n\n")
+    f.write("The news of todays market are the following\n")
+    for item in res2:
+        f.write("%s\n" % item)
+    f.write("\n")
+    f.write("The stocks related to the news are the following\n")
+    for ticker, df in stock_data.items():
+        f.write(f"Prices for {ticker}:\n")
+        f.write(f"{df}\n")
